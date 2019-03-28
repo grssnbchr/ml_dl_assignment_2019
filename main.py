e24385a0afb0b0e36bcdb343c9a74dbf9c04d285
Timo Grossenbacher <wnstnsmth@yahoo.com>
2019-03-28 20:15:18 +0100

    added plot for distribution of class labels in training and validation set

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_predict
 
 from customtransformers import NDStandardScaler, StatisticsExtractor, AddNVDI
 
+
 def main():
     # 1. Load train data
-    (X_train, y_train) = load_train_data(0.1)
+    (X_train, y_train) = load_train_data(0.01)
     sample_size = len(X_train)
 
-    baseline_model()
-    nvdiadder = AddNVDI()
-    X_train = nvdiadder.transform(X_train)
-
-
+    baseline_model(X_train, y_train)
 
     # # 2. Preprocess
     # # Add NVDI
@@ -74,6 +72,7 @@ def main():
     # # 5.-7. [Optional] Further investigations on own emails in text file format
     # # apply_to_own_mails(classifiers)
 
+
 ###############################################################################
 ###   modelling functions
 ###############################################################################
@@ -88,22 +87,32 @@ def baseline_model(X_train, y_train):
     """
     # flatten everything but the first dimension
     X_train = np.transpose(X_train.reshape(-1, X_train.shape[0]))
-    # convert back from one-hot-encoding
-    y_train = np.argmax(y_train, axis=1)
     # split into train and test
-    X_train, X_test, y_train, y_test = train_test_split(
+    X_train, X_val, y_train, y_val = train_test_split(
         X_train,
         y_train,
         test_size=0.2,
         shuffle=True,
         random_state=42
     )
+    # Plot distribution
+    plt.suptitle('relative distributions of classes in train and validation set')
+    plot_label_distribution_bar(y_train, loc='left')
+    plot_label_distribution_bar(y_val, loc='right')
+    plt.legend([
+        'train ({0} photos)'.format(len(y_train)),
+        'test ({0} photos)'.format(len(y_val))
+    ])
+    plt.show()
+    # convert back from one-hot-encoding
+    y_train = np.argmax(y_train, axis=1)
     # Train simple classifier
     rf_clf = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1)
     rf_clf.fit(X_train, y_train)
     # evaluate
-    y_pred = rf_clf.predict(X_test)
-    print('Percentage correct: ', 100 * np.sum(y_pred == y_test) / len(y_test))
+    y_pred = rf_clf.predict(X_val)
+    print('Percentage correct: ', 100 * np.sum(y_pred == y_val) / len(y_val))
+
 
 ###############################################################################
 ###   utility functions
@@ -131,11 +140,59 @@ def load_train_data(fraction=1):
 
 def get_label(y):
     """
+    returns the colloquial label associated with class y
     :param y: the one-hot encoded label
     :return: the colloquial name of the label
     """
     annotations = pd.read_csv('data/deepsat-sat6/sat6annotations.csv', header=None)
+
     return annotations[annotations[np.argmax(y) + 1] == 1][0].item()
+
+
+def plot_label_distribution_bar(y, loc='left', relative=True):
+    """
+    plots a grouped bar chart of the y labels
+    adapted from https://kapernikov.com/tutorial-image-classification-with-scikit-learn/
+    :param y: the train or validation labels as one-hot encoded vectors
+    :param loc: left or right bar
+    :param relative: if True percentages are shown
+    :return:
+    """
+    width = 0.35
+    if loc == 'left':
+        n = -0.5
+    elif loc == 'right':
+        n = 0.5
+
+    # calculate counts per type and sort, to ensure their order
+    unique, counts = np.unique(np.argmax(y, axis=1), return_counts=True)
+    sorted_index = np.argsort(unique)
+    unique = unique[sorted_index]
+
+    if relative:
+        # plot as a percentage
+        counts = 100 * counts[sorted_index] / len(y)
+        ylabel_text = '% count'
+    else:
+        # plot counts
+        counts = counts[sorted_index]
+        ylabel_text = 'count'
+
+    xtemp = np.arange(len(unique))
+
+    plt.bar(xtemp + n * width, counts, align='center', alpha=.7, width=width)
+    # one-hot encode labels and extract colloquial names
+    enc = OneHotEncoder(sparse=False, categories='auto')
+    labels = list(
+        np.apply_along_axis(
+            arr=enc.fit_transform(unique.reshape(len(unique), 1)),
+            func1d=get_label, # extract colloquial names
+            axis=0
+        )
+    )
+    plt.xticks(xtemp, labels)
+    plt.xlabel('land cover class')
+    plt.ylabel(ylabel_text)
 
 
 def plot_sample_images(tX, tY, number=4):
@@ -149,7 +206,7 @@ def plot_sample_images(tX, tY, number=4):
     fig, m_axs = plt.subplots(4, number // 4, figsize=(4, 4))
     for (x, y, c_ax) in zip(tX, tY, m_axs.flatten()):
         print(x.shape)
-        print(x[1:3,1:3, :3])
+        print(x[1:3, 1:3, :3])
         c_ax.imshow(x[:, :, :3].astype(np.uint8),  # since we don't want NIR in the display
                     interpolation='none')
         c_ax.axis('off')
@@ -163,7 +220,7 @@ def plot_sample_channels(tX, tY, number=3):
     :param tY: labels
     :param number: number of different examples
     """
-    assert tX.shape[3] == 5 # assert 5 channels
+    assert tX.shape[3] == 5  # assert 5 channels
     fig, m_axs = plt.subplots(number, 6)
     imgs_to_plot = zip(tX, tY, m_axs[:, 0])
     for i, (x, y, c_ax) in enumerate(imgs_to_plot):
@@ -180,6 +237,7 @@ def plot_sample_channels(tX, tY, number=3):
             r_ax.set_title(f'Ch:{channels[j]}')
 
     plt.show()
+
 
 if __name__ == '__main__':
     main()
